aad5378eb8e56a4e042e96e2677b2f14f8e87cf4
ravindu877 <ravindulaksahn877@gmail.com>
2021-10-09 10:06:31 +0530

    Added Customer form

---
+++ src/lk/ijse/javaApplication/controller/CustomerController.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c)  2020-present, Sri lanka
+ */
+
+package lk.ijse.javaApplication.controller;
+
+import lk.ijse.javaApplication.db.DBConnection;
+import lk.ijse.javaApplication.model.Customer;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+/**
+ * @author Ravindu Lakshan <ravindulakshan877@gmail.com>
+ * @since 10/9/2021
+ **/
+
+public class CustomerController {
+
+    public boolean saveCustomer(Customer customer) throws SQLException, ClassNotFoundException {
+        PreparedStatement pstm= DBConnection.getInstance().getConnection().prepareStatement("INSERT INTO Customer VALUES (?,?,?,?)");
+        pstm.setObject(1,customer.getId());
+        pstm.setObject(2,customer.getName());
+        pstm.setObject(3,customer.getAddress());
+        pstm.setObject(4,customer.getContact());
+        return pstm.executeUpdate()>0;
+    }
+
+}
---
+++ src/lk/ijse/javaApplication/controller/formController/CustomerFormController.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c)  2020-present, Sri lanka
+ */
+
+package lk.ijse.javaApplication.controller.formController;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+/**
+ * @author Ravindu Lakshan <ravindulakshan877@gmail.com>
+ * @since 10/9/2021
+ **/
+public class CustomerFormController {
+    public TextField txtCustID;
+    public TextField txtCustName;
+    public TextField txtCustAddress;
+    public TextField txtCustContact;
+    public Button btnSaveCustomer;
+
+    public void saveCustomerOnAction(ActionEvent actionEvent) {
+    }
+}
