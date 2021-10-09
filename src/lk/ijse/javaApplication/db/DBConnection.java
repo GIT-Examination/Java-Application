34e2a0f27fdc883cde5a4b5c1dc1061c62b96e36
ravindu877 <ravindulaksahn877@gmail.com>
2021-10-09 10:33:16 +0530

    Added DBConnection

---
+++ src/lk/ijse/javaApplication/db/DBConnection.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c)  2020-present, Sri lanka
+ */
+
+package lk.ijse.javaApplication.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * @author Ravindu Lakshan <ravindulakshan877@gmail.com>
+ * @since 10/9/2021
+ **/
+public class DBConnection {
+    private static DBConnection dbConnection;
+    private Connection connection;
+
+    private DBConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.jdbc.Driver");
+        connection= DriverManager.getConnection("jdbc:mysql://localhost:3306/supermarket","root","1234");
+    }
+
+    public static DBConnection getInstance() throws SQLException, ClassNotFoundException {
+        if (dbConnection==null){
+            dbConnection= new DBConnection();
+        }
+        return dbConnection;
+    }
+
+    public Connection getConnection(){
+        return connection;
+    }
+}
